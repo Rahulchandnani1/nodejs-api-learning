144b20a68551b19907528d95b9a62b95569fca06
rahul <rahul@setuschool.com>
2026-03-22 13:21:30 +0530

    mongodb setup

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express=require("express");
 const app=express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 app.use(express.json());
 
 app.get("/test",(req,res)=>{
