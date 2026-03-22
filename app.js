ee08edba373bbd28247645883c184c4a8c8e88e4
rahul <rahul@setuschool.com>
2026-03-22 13:05:48 +0530

    Initial Node.js API setup with Express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+
+app.get("/test",(req,res)=>{
+    res.status(200).json("running");
+})
+app.post("/test",(req,res)=>{
+    const {name,id}=req.body;
+    res.status(200).json({message:{name,id}});
+})
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
